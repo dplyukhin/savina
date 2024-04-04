5a5f68ff11e49a089e4d8440735be6c013fc31f0
Dan Plyukhin <dplyukhin@gmail.com>
2024-04-04 11:25:49 -0500

    Fixed up the benchmark runner script

--- runBenchmarks.py
+++ runBenchmarks.py
@@ -1,52 +1,67 @@
-#!/bin/bash
+import subprocess
+import sys
+import os
 
 benchmarks = {
     "apsp.ApspAkkaGCActorBenchmark": [100, 200, 300, 400, 500],
-    "astar.GuidedSearchAkkaGCActorBenchmark": [10, 20, 30, 40, 50],# -g
-    #"big.BigAkkaGCActorBenchmark", 
-    #"chameneos.ChameneosAkkaGCActorBenchmark", 
+    "astar.GuidedSearchAkkaGCActorBenchmark": [10, 20, 30, 40, 50],
     "count.CountingAkkaGCActorBenchmark": [1000000, 2000000, 3000000, 4000000, 5000000, 6000000],
     "fib.FibonacciAkkaGCActorBenchmark": [22, 23, 24, 25, 26], 
-    #"fjcreate.ForkJoinAkkaGCActorBenchmark", 
-    #"fjthrput.ThroughputAkkaGCActorBenchmark", 
     "nqueenk.NQueensAkkaGCActorBenchmark": [10, 11, 12, 13, 14, 15],
-    #"pingpong.PingPongAkkaGCActorBenchmark", 
     "quicksort.QuickSortAkkaGCActorBenchmark": [500000, 1000000, 1500000, 2000000, 2500000],
     "radixsort.RadixSortAkkaGCActorBenchmark": [50000, 60000, 70000, 80000, 90000, 100000],
     "recmatmul.MatMulAkkaGCActorBenchmark": [1024, 512, 256, 128, 64],
-    #"threadring.ThreadRingAkkaGCActorBenchmark"
 }
 
 opts = {
     "apsp.ApspAkkaGCActorBenchmark": "-n",
     "astar.GuidedSearchAkkaGCActorBenchmark": "-g",
-    #"big.BigAkkaGCActorBenchmark", 
-    #"chameneos.ChameneosAkkaGCActorBenchmark", 
     "count.CountingAkkaGCActorBenchmark": "-n",
     "fib.FibonacciAkkaGCActorBenchmark": "-n",
-    #"fjcreate.ForkJoinAkkaGCActorBenchmark", 
-    #"fjthrput.ThroughputAkkaGCActorBenchmark", 
     "nqueenk.NQueensAkkaGCActorBenchmark": "-n",
-    #"pingpong.PingPongAkkaGCActorBenchmark", 
     "quicksort.QuickSortAkkaGCActorBenchmark": "-n",
     "radixsort.RadixSortAkkaGCActorBenchmark": "-n",
     "recmatmul.MatMulAkkaGCActorBenchmark": "-n",
-    #"threadring.ThreadRingAkkaGCActorBenchmark"
 }
-alt_gcs = ["NoProtocol"]#, "wrc"]
-styles = ["on-block", "wave"]
+
 iter=30
 
-import subprocess
+# Check if there are any .csv files in the directory. If the "--append" flag is not used, abort.
+for file in os.listdir('.'):
+    if file.endswith('.csv') and "--append" not in sys.argv:
+        print("There are .csv files in the directory. Either remove them or re-run with the --append flag. Aborting.")
+        sys.exit()
+
 for benchmark in benchmarks.keys():
     opt = opts[benchmark]
     for param in benchmarks[benchmark]:
+        classname = "edu.rice.habanero.benchmarks." + benchmark
 
-        for gc in alt_gcs:
-            filename = f"{benchmark}-n{param}-{gc}.csv"
-            subprocess.run(["sbt", f"-Duigc.protocol={gc}", f'"runMain {benchmark} -iter {iter} -filename {filename} {opt} {param}"'])
+        # No GC
+        filename = f"{benchmark}-n{param}-nogc"
+        subprocess.run(
+            ["sbt",
+             f"-Duigc.engine=manual",
+             f'runMain {classname} -iter {iter} -filename {filename}.csv {opt} {param}'])
 
-        for style in styles:
-            filename = f"{benchmark}-n{param}-Monotone-{style}.csv"
-            subprocess.run(["sbt", f"-Dgc.crgc.collection-style={style}", f"-Duigc.protocol=Monotone", f'"runMain {benchmark} -iter {iter} -filename {filename} {opt} {param}"'])
+        # WRC
+        filename = f"{benchmark}-n{param}-WRC"
+        subprocess.run(
+            ["sbt",
+             f"-Duigc.engine=mac", "-Duigc.mac.cycle-detection=off",
+             f'runMain {classname} -iter {iter} -filename {filename}.csv {opt} {param}'])
+
+        # CRGC on-block
+        filename = f"{benchmark}-n{param}-crgc-onblock"
+        subprocess.run(
+            ["sbt",
+             f"-Dgc.crgc.collection-style=on-block", f"-Duigc.engine=crgc",
+             f'runMain {classname} -iter {iter} -filename {filename}.csv {opt} {param}'])
+
+        # CRGC wave
+        filename = f"{benchmark}-n{param}-crgc-wave"
+        subprocess.run(
+            ["sbt",
+             f"-Dgc.crgc.collection-style=wave", f"-Duigc.engine=crgc",
+             f'runMain {classname} -iter {iter} -filename {filename}.csv {opt} {param}'])
 
